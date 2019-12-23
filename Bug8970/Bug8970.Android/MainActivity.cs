23c2820e931e4cdb3c9da21afe5c1a8e5a9e0bf5
Daniel Domengeaux <danield@supa-fly.org>
2019-12-23 14:21:34 -0600

    Initial Commit

---
+++ Bug8970/Bug8970.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using FFImageLoading.Forms.Platform;
+using Xamarin.Forms;
+
+namespace Bug8970.Android
+{
+    [Activity(Label = "Bug8970", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Forms.SetFlags("CollectionView_Experimental");
+            CachedImageRenderer.Init(true);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ Bug8970/Bug8970/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bug8970
+{
+    public class ViewModel
+    {
+        public IList<string> Images =>
+            new List<string>()
+            {
+                "http://lorempixel.com/400/200/",
+                "http://lorempixel.com/400/200/",
+                "http://lorempixel.com/400/200/",
+                "http://lorempixel.com/400/200/"
+            };
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
